9bee58d7501359ddf8f2af69ded38152e63c5fe2
alexandretta <alexsalazar21@gmail.com>
2023-02-22 15:24:05 -0500

    Added petition and character renderization garanted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import { allCharacters } from '../functions/functions';
 
 const Home = () => {
+  const [characters, setCharacters] = useState(null)
+
+  useEffect(() => {
+    allCharacters(setCharacters)
+  }, [])
   return (
-  <div>INICIO</div>
+  <>
+    {characters != null ? (
+      characters.map(character => (
+        <div key={character.id}>
+          <a href={`/character/${character.id}`}>{character.name}</a>
+        </div>
+      ))
+    ) : ('No hay personajes')}
+  </>
   );
 };
 export default Home;
--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const allCharacters = async () => {
-  
+const allCharacters = async (state) => {
+  const petition = await axios.get('https://rickandmortyapi.com/api/character')
+  state(petition.data.results);
 }
+
+export {allCharacters};
